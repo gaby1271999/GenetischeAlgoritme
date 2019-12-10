4c9e3d946ac9870c08c8e2e4905461a30ff38406
Gabriel <gabriel.dhondt@hotmail.com>
2019-12-10 18:11:13 +0100

    Main format en comments aanmaken.

--- src/genetischealgoritmen/Main.java
+++ src/genetischealgoritmen/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package genetischealgoritmen;
 
 import java.io.File;
@@ -11,10 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Main class van het programma
+ * 
+ * @author Aäron Vanhoecke, Felix Capon en Gabriel D'Hondt
+ * @version 1.0
+ * @since 1.0
+ */
 public class Main {
 
     protected static List<Stad> beschikbareSteden;
 
+    /**
+     * Start van het programma. Leest het bestand steden.txt in en geeft de meest 
+     * toepasselijke Chromosoom weer.
+     * 
+     * @param args 
+     */
     public static void main(String[] args) {
  
         try {
@@ -23,38 +31,45 @@ public class Main {
             Populatie p = new Populatie();
             
             System.out.println("Een fitnesswaarde < 1 is beter t.o.v. het gemiddelde.\n");
-
-
             System.out.println("Eerste (willekeurige) generatie heeft als beste chromosoom:\n" + p.getBesteChromosoom() + "\n");
-           
             
+            System.out.print("test/r");
             for (int i = 0; i < Consts.AANTAL_GENERATIES; i++) {
                 p.maakNieuweGeneratie();
-                //System.out.println("beste: " + p.getBesteChromosoom());
+                System.out.print("test\r");
             }
 
             Chromosoom beste = p.getBesteChromosoom();
             System.out.println("\nHet beste chromosoom uit de eindpopulatie is:\n" + beste);
-
         } catch (FileNotFoundException err) {
             System.out.println("Fout. Geef nieuwe bestandnaam: ");
         }
-
     }
 
-
+    /**
+     * Leest een bestand in en maakt een lijst van de steden en locaties die zich in
+     * het bestand bevinden 
+     * 
+     * @param bestandsnaam
+     * @throws FileNotFoundException 
+     */
     private static void leesSteden(String bestandsnaam) throws FileNotFoundException {
         beschikbareSteden = new ArrayList<>();
         Scanner sc = new Scanner(new File(bestandsnaam));
+        
         int aantalSteden = sc.nextInt();
-        Consts.AANTAL_GENEN = aantalSteden + 1; // bekijk dit nog eens
-        sc.nextLine(); // '\n' weg
+        Consts.AANTAL_GENEN = aantalSteden + 1;
+        
+        sc.nextLine();
+        
         int teller = 0;
         while (sc.hasNext() && teller < aantalSteden) {
             String[] lijn = sc.nextLine().split(":");
             beschikbareSteden.add(new Stad(Integer.parseInt(lijn[0]), Integer.parseInt(lijn[1]), lijn[2]));
+            
             teller++;
         }
+        
         sc.close();
     }
 }
