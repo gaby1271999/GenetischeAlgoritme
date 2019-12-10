2402f58353e9b1e4cebd69270f0370d56591a523
Gabriel <gabriel.dhondt@hotmail.com>
2019-12-10 16:39:19 +0100

    Gen format en comments aanmaken.

--- src/genetischealgoritmen/Gen.java
+++ src/genetischealgoritmen/Gen.java
@@ -1,43 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package genetischealgoritmen;
 
-
 /**
- *
- * @author Felix Capon, Gabriel D'Hondt, Aäron Vanhoecke
+ * Is een Gen deze houdt de stad en de volgnummer bij
+ * 
+ * @author Aäron Vanhoecke, Felix Capon en Gabriel D'Hondt
+ * @version 1.0
+ * @since 1.0
  */
 public class Gen {
 
     private Stad stad;
     private int volgnr;
 
+    /**
+     * Maakt een nieuwe Gen aan
+     * 
+     * @param stad
+     * @param volgnr 
+     */
     public Gen(Stad stad, int volgnr) {
         this.stad = stad;
         this.volgnr = volgnr;
-
     }
     
+    /**
+     * Maakt een copy van een bestaande Gen aan
+     * 
+     * @param gen 
+     */
      public Gen(Gen gen){
-        this.stad = gen.stad; //er wordt nooit iets veranderd aan een stad, dus dit mag een referentie zijn
+        this.stad = gen.stad;
         this.volgnr = gen.volgnr;
     }
 
+    /**
+     * @return van het Stad object van het Gen object
+     */
     public Stad getStad() {
         return stad;
     }
 
+    /**
+     * @return van de volgnummer van het Gen object
+     */
     public int getVolgnr() {
         return volgnr;
     }
 
+    /**
+     * past de volgnummer van het Gen object aan
+     * 
+     * @param volgnr 
+     */
     public void setVolgnr(int volgnr) {
         this.volgnr = volgnr;
     }
     
+    /**
+     * Verandert het volgnummer met de waarde die zowel positief of negatief mag zijn.
+     * 
+     * @param waarde 
+     */
     public void veranderVolgnr(int waarde) {
         /*this.volgnr += waarde;
         if (volgnr >= 140) {
@@ -53,6 +76,9 @@ public class Gen {
         }
     }
 
+    /**
+     * @return de naam van het Stad object.
+     */
     @Override
     public String toString() {
         return stad.getNaam();
